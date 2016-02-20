6ce770c0564997d5290c6c277d444e11217b89de
michael <cihv2@163.com>
2016-02-20 17:19:26 +0800

    第四章：光照1 效果代码

---
+++ SolarSystem_chapter4_black_sphere/SolarSystem/OpenGLSolarSystemController.m
@@ -0,0 +1,28 @@
+//
+// Created by michael on 2/19/16.
+// Copyright (c) 2016 michaelfx. All rights reserved.
+//
+
+#import "OpenGLSolarSystemController.h"
+
+@implementation OpenGLSolarSystemController
+
+- (id)init {
+    [self initGeometry];
+    return self;
+}
+
+- (void)initGeometry {
+    m_Earth = [[Planet alloc] init:10 slices:10 radius:1.0 squash:1.0];
+}
+
+- (void)execute {
+    static GLfloat angle = 0;
+    glLoadIdentity();
+    glTranslatef(0.0, -0.0, -3.0);
+    glRotatef(angle, 0.0, 1.0, 0.0);
+    [m_Earth execute];
+    angle += .5;
+}
+
+@end
